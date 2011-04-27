9f764268145acfe0f98711164aeb3607ef07fb09
Chris <cjw7488@gmail.com>
2011-04-27 12:08:11 -0500

    Finished user following

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,6 @@
 class RelationshipsController < ApplicationController
-  
+  before_filter :authenticate
+
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
@@ -12,11 +13,9 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
     end
   end
-  
 end
